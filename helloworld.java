7f75b51737ef6070bd46ea947a6f1b5d3dcf14d3
joyceliaoxy <joyceliaoxy@gmail.com>
2019-02-18 15:54:13 +0800

    First commit

---
+++ helloworld.java
@@ -0,0 +1,13 @@
+// joyce
+// show hello world
+// jun 11 2017
+public class helloworld
+{ public static void main(String args[])
+    {
+        System.out.println("hello world!");
+        int a=10;
+        int b=20;
+        int result=a+b;
+        System.out.println("result is" +result);
+    }
+}
